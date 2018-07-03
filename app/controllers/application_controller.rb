4d2034c6b7541dc5012787b7a311c71a5d1a1e8d
Joe Darling <joe.darling@bt.com>
2018-07-03 17:10:56 +0100

    Add admin and company to the user creation invite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite, keys: %i[admin company_id])
+  end
 end
--- factories/companies.rb
+++ factories/companies.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :company do
-    name Faker::Company.name
+    name { Faker::Company.name }
   end
 end
